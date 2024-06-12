aec462c3e3a5f4a0fbd7acd26af12944ec894102
Zai Santillan <zaisantillan14@gmail.com>
2024-06-12 15:35:53 +0800

    chore(card): image alt and animation

--- src/app/component/card.tsx
+++ src/app/component/card.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 
 export default function Card() {
   return (
-    <div className='group relative h-[522px] w-[384px] bg-neutral-white stroke-black rounded-2xl p-6 space-y-6 border border-neutral-black'>
+    <div className='group relative h-[522px] w-[384px] bg-white stroke-black rounded-2xl p-6 space-y-6 border border-neutral-black'>
       <Image
-        src={'/images/illustration-article.svg'}
-        alt='illustration article'
+        src='/images/illustration-article.svg'
+        alt='Illustration of the article'
         width={336}
         height={200}
         className='h-[200px] w-full rounded-xl'
@@ -17,7 +17,7 @@ export default function Card() {
         </div>
         <p className='text-sm'>Published 21 Dec 2023</p>
         <h1 className='group-hover:text-primary-yellow text-2xl font-extrabold'>
-          HTML & CSS foundations
+          HTML & CSS Foundations
         </h1>
         <p className='text-neutral-grey'>
           These languages are the backbone of every website, defining structure,
@@ -27,16 +27,16 @@ export default function Card() {
 
       <div className='flex items-center gap-3'>
         <Image
-          src={'/images/image-avatar.webp'}
-          alt='profile'
+          src='/images/image-avatar.webp'
+          alt='Profile picture of Greg Hooper'
           width={32}
           height={32}
-          className='size-8'
+          className='rounded-full'
         />
         <p className='text-sm font-bold'>Greg Hooper</p>
       </div>
 
-      <div className='absolute bg-neutral-black h-[522px] w-[384px] rounded-2xl -top-4 left-2 -z-10 group-hover:-top-2 group-hover:left-4' />
+      <div className='absolute bg-neutral-black h-[522px] w-[384px] rounded-2xl -top-4 left-2 -z-10 group-hover:-top-2 group-hover:left-4 transition-all duration-300' />
     </div>
   )
 }
